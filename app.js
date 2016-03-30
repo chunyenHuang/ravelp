f6ffbc8c3da4c22c433b56fd495fe66712657d90
John Huang <little78926@gmail.com>
2016-03-30 12:14:11 -0700

    Complete the user login and application.#7

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// Database
+var users = [{
+  username: "test",
+  password: "123",
+  firstname: "Tesla",
+  lastname: 'Ola',
+  email: '123123@gmail.com',
+  phone: '123-123-1233',
+  address: '100 JD St., Irvine, CA92603',
+  business: true
+}, {
+  username: "business",
+  password: "123",
+  firstname: "Steve",
+  lastname: 'Ma',
+  email: '123123@gmail.com',
+  phone: '123-123-1233',
+  address: '100 JD St., Irvine, CA92603',
+  business: false
+}];
+
+function User(username, password, firstname, lastname, email, phone, address, business){
+  this.username = username;
+  this.password = password;
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.email = email;
+  this.phone = phone;
+  this.business = business;
+}
+
+// Modules
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
@@ -6,10 +38,90 @@ var cookieParser = require('cookie-parser');
 var port = 3000;
 var app = express();
 
+// global functions
+function sessionToken(length){
+  var token = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(var x=0; x < length; x++){
+    token += possible.charAt(Math.floor(Math.random() * possible.length)+1);
+  }
+  return token;
+}
+
+var sessions =[];
+
+function Session(token, username){
+  this.token = token;
+  this.username = username;
+}
+
+// Routes
 app.use(express.static('./public/assets'));
+app.use(cookieParser());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/login', jsonParser, function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var remember = req.body.remember;
+  var business = req.body.business;
+  var match = _.where(users, {username: username});
+  if (match.length>0){
+    if (match[0].password === password){
+      if (remember) {
+        var token = sessionToken(50);
+        res.cookie('sessionTokenForRavelp', token);
+        sessions.push(new Session(token, match[0].username));
+      }
+      res.json(match[0]);
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.post('/newuser', jsonParser, function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var address = req.body.address;
+  var business = req.body.business;
+  var match = _.where(users, {username: username});
+  if (match.length>0){
+    res.sendStatus(403);
+  } else {
+    var newUser = new User(username, password, firstname, lastname, email, phone, address, business);
+    users.push(newUser);
+    var currentUser = _.where(users, {username: username});
+    var token = sessionToken(50);
+    res.cookie('sessionTokenForRavelp', token);
+    sessions.push(new Session(token, currentUser[0].username));
+    res.json(currentUser[0]);
+  }
+})
+
+app.get('/login', function(req, res){
+  var currentToken = req.cookies.sessionTokenFor85;
+  var matchSession = _.where(sessions, {token: currentToken});
+  if (matchSession.length>0){
+    var currentUser = _.where(users, {username: matchSession[0].username})
+    res.json(currentUser[0]);
+  } else {
+    res.redirect('/');
+  }
+})
+
+app.get('/logout', function(req, res){
+  res.clearCookie('sessionTokenForRavelp');
+  res.redirect('/');
 })
 
 app.listen(port, function(){
--- public/assets/default.js
+++ public/assets/default.js
@@ -5,7 +5,19 @@ function filterInt(value) {
   return NaN;
 }
 
+function removeAllChild(nodeName){
+  while (nodeName.firstChild) {
+      nodeName.removeChild(nodeName.firstChild);
+  }
+}
+
+function clearPage(){
+  removeAllChild(main);
+  loginForm.classList.add('hidden');
+  newUserForm.classList.add('hidden');
+}
 // Elements
+var main = document.getElementById('main');
 var loginForm = document.getElementById('login-form');
 var newUserForm = document.getElementById('user-application');
 
@@ -23,6 +35,13 @@ document.body.addEventListener('click', function(event){
     newUserForm.classList.remove('hidden');
     loginForm.classList.add('hidden');
   }
+  if (type==='logout'){
+    var XHR = new XMLHttpRequest();
+    XHR.open('get','/login');
+    newUserForm.classList.add('hidden');
+    loginForm.classList.remove('hidden');
+    removeAllChild(main);
+  }
 })
 
 document.body.addEventListener('submit', function(e){
@@ -31,15 +50,15 @@ document.body.addEventListener('submit', function(e){
   var type = event.target.getAttribute('data-type');
 
   if (type==='login'){
-    var inputUsername = document.getElementById('username');
-    var inputPassword = document.getElementById('password');
+    var inputUsername = document.getElementById('input-username');
+    var inputPassword = document.getElementById('input-password');
     var inputRemember = document.getElementById('remember-me');
     var inputBusiness = document.getElementById('business');
     var loginUser = {
       username: inputUsername.value,
       password: inputPassword.value,
-      remember: inputRemember.value,
-      business: inputBusiness.value
+      remember: inputRemember.checked,
+      business: inputBusiness.checked,
     };
     console.log(loginUser);
     var payload = JSON.stringify(loginUser);
@@ -48,8 +67,64 @@ document.body.addEventListener('submit', function(e){
     XHR.setRequestHeader('content-type', 'application/json');
     XHR.send(payload);
 
+    var msg = document.getElementById('error-msg');
     XHR.onload = function(){
-      console.log(XHR.responseText);
+      if (XHR.status === 404) {
+        msg.textContent = '(username not exists)';
+      } else if (XHR.status === 403){
+        msg.textContent = '(wrong password)';
+      } else {
+        loginForm.classList.add('hidden');
+        console.log(XHR.responseText);
+
+        var logout = document.createElement('button');
+        logout.setAttribute('data-type', 'logout');
+        logout.setAttribute('data-id', 'nan');
+        logout.textContent = 'logout';
+        main.appendChild(logout);
+      }
+    }
+  }
+  if (type==='new-user'){
+    var username = document.getElementById('new-username');
+    var password = document.getElementById('new-password');
+    var firstname = document.getElementById('new-firstname');
+    var lastname = document.getElementById('new-lastname');
+    var email = document.getElementById('new-email');
+    var phone = document.getElementById('new-phone');
+    var address = document.getElementById('new-address');
+    var business = document.getElementById('new-business');
+
+    var newUser = {
+      username: username.value,
+      password: password.value,
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      phone: phone.value,
+      address: address.value,
+      business: business.checked,
+    };
+    console.log(newUser);
+    var payload = JSON.stringify(newUser);
+    var XHR = new XMLHttpRequest();
+    XHR.open('POST', '/newuser');
+    XHR.setRequestHeader('content-type', 'application/json');
+    XHR.send(payload);
+
+    var msg = document.getElementById('error-msg-2');
+    XHR.onload = function(){
+      if (XHR.status === 403) {
+        msg.textContent = '(username is already taken)';
+      } else {
+        clearPage();
+
+        var logout = document.createElement('button');
+        logout.setAttribute('data-type', 'logout');
+        logout.setAttribute('data-id', 'nan');
+        logout.textContent = 'logout';
+        main.appendChild(logout);
+      }
     }
   }
 })
